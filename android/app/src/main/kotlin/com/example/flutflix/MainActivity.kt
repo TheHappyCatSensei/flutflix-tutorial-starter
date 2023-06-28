4643a44b28733fcebc02dfe3ccce67f7fceeef53
HappyCat <happycat.dev@gmail.com>
2023-06-28 22:26:27 +0000

    initial commit

---
+++ android/app/src/main/kotlin/com/example/flutflix/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutflix
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/colors.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+class Colours {
+  static const scaffoldBgColor = Color(0xFF23272E);
+  static const ratingColor = Color(0xFFFFC107);
+}
